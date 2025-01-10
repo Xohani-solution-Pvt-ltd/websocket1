0f15e36f564dbe70992422b9a158381e119a3212
Himanshu <himanshu.m@xohani.com>
2025-01-10 16:02:07 +0530

    updates

--- websocket/index.js
+++ websocket/index.js
@@ -1,11 +1,36 @@
-import { WebSocketServer } from "ws";
+// import { WebSocketServer } from "ws";
+
+// const wss = new WebSocketServer({ port: 8080 });
+
+// wss.on("connection", function connection(ws) {
+//   ws.on("message", function message(data) {
+//     console.log("received: %s", data);
+//     //ws.send("Akash");
+//   });
+//   ws.send("Akash");
+// });
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
+  console.log("New connection established");
+
   ws.on("message", function message(data) {
     console.log("received: %s", data);
-    //ws.send("Akash");
+    ws.send("Message received: " + data); // Acknowledge the message
   });
-  ws.send("Akash");
+
+  ws.on("error", (err) => {
+    console.error("WebSocket error:", err);
+  });
+
+  ws.on("close", () => {
+    console.log("Connection closed");
+  });
+
+  ws.send("Welcome to the server!");
+});
+
+wss.on("error", (err) => {
+  console.error("WebSocket Server error:", err);
 });
